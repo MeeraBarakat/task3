4424f43e0ab3e8dbaed0db2f163054a5a6e167b8
Meera <meera.gh.barakat@gmail.com>
2022-11-28 21:44:00 +0200

    burger-selection

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -3,8 +3,8 @@ import map from 'lodash/map';
 import findIndex from 'lodash/findIndex';
 import find from 'lodash/find';
 import Ingredient from "./Ingredient";
-import{useDispatch,useSelector} from "react-redux"
-import {addIng,setIng} from "../features/burger"
+import{useDispatch,useSelector} from "react-redux";
+import {addIng,setIng} from "../features/burger";
 import { useNavigate } from "react-router-dom";
 import menu from '../data/Predefined-burgers.json';
 import Ingredients from '../data/burger.json';
@@ -15,29 +15,43 @@ function ControlPanel() {
     const navigate=useNavigate(null);
     let Ing=[];
     let price=0;
+    let myburger='';
+
+    const compareArrays = (_arr1, _arr2)=> {
+        if (!Array.isArray(_arr1)|| !Array.isArray(_arr2)|| _arr1.length !== _arr2.length) {
+            return false;
+          }
+        const arr1 = _arr1.concat().sort();
+        const arr2 = _arr2.concat().sort();
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+             }
+        }
+        return true;
+    }
 
     return (
         <div className="ControlPanel">
             <div style={{width:'100%'}}>
                 <span className='header'>
                   Choose a burger from our menu
-                </span>
-                <select aria-label='Ingredients' className='options' onChange={(e)=> {
-                    Ing=[];
-                    const burger=find(menu,x => x.name === e.target.value);
-                    dispatch(setIng(burger.ingredients));
-                }}>
-                    <option>choose</option>
-                    {map(menu,(burger)=>
+                </span>  
+                {map(menu,(burgerr)=>
                         {
-                            return <option key={burger.name} value={burger.name}>{burger.name}</option>
+                            return <div onClick={()=>{
+                                Ing=[];
+                                myburger=find(menu,x => x.name === burgerr.name);
+                                dispatch(setIng(myburger.ingredients));
+                            }} style={{backgroundImage: "url("+burgerr.path+")",
+                            border:compareArrays(burger,find(menu,x => x.name === burgerr.name).ingredients) ? '2px solid rgb(15, 125, 235)' :0}} className='Cart'>
+                                <span className='burger-name'>{burgerr.name}</span></div>
                         })
                     }
-                </select>
-                <span className='header'>
+                <span style={{marginTop:'40px'}} className='header'>
                     Add Ingredients to your burger
                 </span>
-                <select aria-label='Ingredients' className='options' onChange={(e)=> dispatch(addIng(e.target.value))}>
+                <select aria-label='Ingredients' className='options' onChange={(e)=> {dispatch(addIng(e.target.value))}}>
                     <option>choose</option>
                     {map(Ingredients,(Ing,idx)=>{
                         if(idx !== 0 && idx !== 1 && findIndex(burger,x => x ===Ing.name) === -1)
